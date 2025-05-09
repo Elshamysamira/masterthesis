1fe27aa2a93de76c11129ffd6e391ad623026b53
Samira El-Shamy <138245239+Elshamysamira@users.noreply.github.com>
2025-05-09 18:33:51 +0000

    new experiment added

--- Information_Retrieval_Querying_Noisy_fixed.py
+++ Information_Retrieval_Querying_Noisy_fixed.py
@@ -57,7 +57,7 @@ embeddings = OpenAIEmbeddings(model="text-embedding-ada-002")
 
 
 # Load documents from a folder
-folder_path = "documents/noisy_documents_moderate_english"  # Path to your folder with .txt files
+folder_path = "documents/noisy_documents_severe_english"  # Path to your folder with .txt files
 file_paths = list(Path(folder_path).rglob("*.txt"))
 
 # Load documents from each .txt file in the folder
@@ -160,7 +160,7 @@ def load_noisy_queries(file_path):
         queries = [line.strip() for line in file.readlines()]
     return queries
 
-noisy_queries_file = "queries/queries_german/questions_german.txt"  # Adjust path if necessary
+noisy_queries_file = "queries/noisy_queries_severe_german/noisy_queries_severe_german.txt"  # Adjust path if necessary
 noisy_queries = load_noisy_queries(noisy_queries_file)
 
 # Function to compute Hits@K
@@ -265,10 +265,10 @@ for i, query in enumerate(noisy_queries, 1):
 df = pd.DataFrame(retrieval_data)
 
 # Save to CSV inside "results" folder
-output_folder = "results_openai/crosslingual/moderate_errors"
+output_folder = "results_openai/crosslingual/severe_errors"
 os.makedirs(output_folder, exist_ok=True)  # Create folder if it doesn't exist
 
-output_csv = os.path.join(output_folder, "german_english_clean_queries_noisy_documents_moderate_results.csv")
+output_csv = os.path.join(output_folder, "german_english_noisy_queries_noisy_documents_severe_results.csv")
 df.to_csv(output_csv, index=False, encoding="utf-8-sig", sep=";")
 
 print(f"\n Retrieval results saved to {output_csv} successfully!")
